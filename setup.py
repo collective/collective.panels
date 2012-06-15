b5a9c7d25b39aed66aaaea11cb70e8ec3aae852f
Malthe Borch <mborch@gmail.com>
2012-06-15 15:13:03 +0200

    Preparing release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read().\
            decode('utf-8')
 
-version = '1.3'
+version = '1.3.1'
 
 setup(name='collective.panels',
       version=version,
