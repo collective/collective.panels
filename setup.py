02bfa77663fb4f6d3c4d8cae3b3e4d018d44801d
Bo Simonsen <bo@geekworld.dk>
2013-11-04 10:10:44 +0100

    Preparing release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read().\
            decode('utf-8')
 
-version = '1.5'
+version = '1.6'
 
 setup(name='collective.panels',
       version=version,
